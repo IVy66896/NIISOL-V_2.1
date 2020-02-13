70897fa02892bc8ccfe1905106a257506ee4c877
peter0817 <59242083+peter0817@users.noreply.github.com>
2020-02-13 06:56:19 +0000

    Create TableOperation.cs

---
+++ NIISOSL/TableOperation.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T00SharedLibraryDotNet20
+{
+	public enum TableOperation
+	{
+		Select,
+		Insert,
+		Delete,
+		Update
+	}
+}
